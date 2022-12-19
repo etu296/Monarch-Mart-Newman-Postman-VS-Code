8becf8daf546f9dbbf322f1507e20361cd6b5f4d
etu296 <etumahmuda@gmail.com>
2022-12-19 14:25:27 +0600

    monarch mart newman html report generate

---
+++ report.js
@@ -0,0 +1,19 @@
+const newman = require('newman')
+newman.run({
+collection:require('./collection/Monarch Mart.postman_collection.json'),
+
+reporters: 'htmlextra',
+iterationCount: 1,
+reporter: {
+	htmlextra:{
+	export:'./Reports/report.html', //if not specified,the file will be written to `newman/`
+
+}
+}
+
+}, function(err){
+	if (err) {throw err;}
+	console.log('collection run complete');
+}
+
+);
